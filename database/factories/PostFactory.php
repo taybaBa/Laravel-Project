10b8a6f9727d136566b6990da6d025a7ea5f04ba
Tayyba Bashir <taybabashir20@gmail.com>
2024-07-09 09:03:26 +0500

    create post table seed data into that

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class PostFactory extends Factory
 {
     protected $model = Post::class;
+
     /**
      * Define the model's default state.
      *
